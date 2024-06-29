fe6a5578523dc76834c19237bf6817f9a0447d09
Darek Nowak <racingchamp007@gmail.com>
2024-06-28 21:35:35 -0500

    Initial push. Added icon image and refactored file names.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Example
-An example greeter plugin
+# Japanese Translator Plugin for Runelite
+
+## Overview
+
+The Japanese Translator Plugin is a custom plugin for the Runelite client that translates in-game English text to Japanese. This plugin is especially useful for players who are learning Japanese and want to immerse themselves in the language while playing Old School RuneScape (OSRS).
+
+## Features
+
+- Translates in-game English text to Japanese.
+- Helps learners of Japanese improve their language skills in a practical, immersive environment.
+- Easy to enable and configure within the Runelite client.
+
+
--- src/main/java/com/jptranslator/JapanesePlugin.java
+++ src/main/java/com/jptranslator/JapanesePlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.jptranslator;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class JapanesePlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private appConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	appConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(appConfig.class);
 	}
 }
